7ede672ba718ca9492676c8f56ed8098a8fe6c99
abdussamadsaiful7 <samadsaifullah11@gmail.com>
2023-03-14 02:18:23 +0600

    practice indexOf

--- function.js
+++ function.js
@@ -38,4 +38,30 @@ const totalNumber = getNumber(33);
 const add2 = (first, second) => first + second;
 
 const result2 = add2(10, 20)
-console.log(result2);
+//console.log(result2);
+
+
+const add4 =(first, second) => (first * second) + 20;
+const result4 = add4(10, 20);
+//console.log(result4);
+
+
+const multiply = (a, b) => a * b;
+const multiplyResult = multiply(100, 200);
+//console.log(multiplyResult);
+
+function getMultiply(num1, num2){
+    const total = num1 * num2;
+    return total;
+}
+const finalResult = getMultiply(100, 300);
+//console.log(finalResult);
+
+const numbers= [10, 20, 30, 40, 50];
+const Result = numbers.indexOf(30);
+//console.log(Result);
+
+
+const beasts = ['ant', 'bison', 'camel', 'duck', 'bison'];
+
+//console.log(beasts.indexOf('duck'));
