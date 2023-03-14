690b02a0829953b491f31e02c191bd7048dd1e5b
abdussamadsaiful7 <samadsaifullah11@gmail.com>
2023-03-14 11:49:20 +0600

    practice spread operator

--- es6.js
+++ es6.js
@@ -9,7 +9,7 @@ const about = `My name is ${student.name},
 age of ${student.age}, I have ${numbers[4]}, 
 and Also liked reading book such as ${student.book[2]} and ${student.book[3]}`;
 
-console.log(about);
+//console.log(about);
 
 //arrow function
 
@@ -17,11 +17,11 @@ const getFiftyFive = () => 55;
 console.log(getFiftyFive());
 
 const isEven = x => x % 2 === 0;
-console.log(isEven(10001));
+//console.log(isEven(10001));
 
 const addNumbers = (x, y, z) => x+y+z;
 const resulT = addNumbers(200, 300, 400);
-console.log(resulT);
+//console.log(resulT);
 
 
 // multi arrow function
@@ -31,4 +31,22 @@ const doMath = (num1, num2) =>{
     return sum;
 }
 const output = doMath(111, 222);
-console.log(output);
+//console.log(output);
+
+//spread operator,
+
+// const Numbers = [89, 35, 98, 12]
+// Numbers.push(9999999)
+// const newNumber =[...Numbers, 3333, 4444];
+// //console.log(newNumber);
+// //console.log(Numbers);
+
+// const newNumber2 =[...Numbers];
+// console.log(newNumber2);
+
+numbersList = [89, 35, 98, 12];
+const newNumber = numbersList;
+numbersList.push(10000000000);
+
+const newNumber2 = [...numbersList];
+console.log(newNumber2);
