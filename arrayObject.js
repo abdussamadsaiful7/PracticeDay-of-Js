d861cb39e59e01c282a2bb8bd2bb92db90f27f3b
abdussamadsaiful7 <samadsaifullah11@gmail.com>
2023-03-14 12:17:21 +0600

    added  array of object

---
+++ arrayObject.js
@@ -0,0 +1,19 @@
+const products = [
+    {name: 'laptop', price: 32000, brand: 'levono', color:'silver'},
+    {name: 'phone', price: 22000, brand: 'HTC', color:'white'},
+    {name: 'watch', price: 7000, brand: 'Casio', color:'yellow'},
+    {name: 'Sunglass', price: 500, brand: 'RoyBand', color:'black'},
+    {name: 'camera', price: 9000, brand: 'Canon', color:'gray'}
+];
+
+const brands = products.map(product => product.brand);
+console.log(brands);
+
+const names = products.map(product => product.name);
+console.log(names);
+
+const prices =  products.map(x => x.price);
+console.log(prices);
+
+const colors = products.map(samad => samad.color);
+console.log(colors);
